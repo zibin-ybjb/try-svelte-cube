92cb422c8bb2a9060101e79baeeb16d3a98e1548
liuzibin <liuzibin@kuaishou.com>
2021-12-17 18:50:38 +0800

    初始化

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import * as THREE from 'three';
+	import * as SC from 'svelte-cubed'
+</script>
+
+<main>
+  <SC.Canvas>
+    <SC.Mesh geometry={new THREE.BoxGeometry()} />
+    <SC.PerspectiveCamera position={[3, 1, 3]} />
+  </SC.Canvas>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+
+</style>
